829cb8d349fab1b2ab1195e83fb6b2a98e3b4e9c
Soren Williams <sorenw@khanlabschool.org>
2018-09-06 16:14:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+if (Date.now() > 1536854400000) {
+	var index = 2;
+}
+if (Date.now() > 1536249600000) {
+	var index = 1;
+}
+if (Date.now() < 1536249600000) {
+	var index = 0;
+}
+
+var prompts = [
+	'You have been named president for a day. What do you do?'
+];
+
+if (index !== 0) {
+  document.getElementById('prompt').innerHTML = prompts[index - 1];
+  document.getElementById('submit').disabled = false;
+}
+if (localStorage.id !== undefined) {
+  document.getElementById('submit').disabled = true;
+  document.getElementById('submit').innerHTML = 'You have already submitted a response';
+}
+
+document.getElementById('responseInputForm').addEventListener('submit', saveResponse);
+
+function saveResponse(e) {
+  if (document.getElementById('responseInput').value === '' || document.getElementById('nameInput').value === '' || document.getElementById('emailInput').value === '') {
+    alert('Please fill out all fields before submitting.');
+  } else {
+    var responseDesc = document.getElementById('responseInput').value;
+    var responseAuthor = document.getElementById('nameInput').value;
+    var responseEmail = document.getElementById('emailInput').value;
+    var response = {
+      description: responseDesc,
+      email: responseEmail,
+      date: Date.now()
+    };
+
+    firebase.database().ref('prompt ' + index + '/responses/' + responseAuthor).set(response);
+    // firebase.database().ref('prompt ' + index + '/ips').set(JSON.parse('https://api.ipify.org?format=json').ip);
+    document.getElementById('submit').disabled = true;
+    document.getElementById('submit').innerHTML = 'Thanks!';
+    document.getElementById('responseInput').value = '';
+    document.getElementById('nameInput').value = '';
+    document.getElementById('emailInput').value = '';
+    localStorage.id = Chance.guid();
+  }
+  e.preventDefault();
+}
