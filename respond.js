2d53f5f40c285f603dc387beb43ab9a1a2e2a251
Soren Williams <sorenw@khanlabschool.org>
2018-09-07 17:12:14 -0700

    Create respond.js

---
+++ respond.js
@@ -0,0 +1,20 @@
+var respondHTML = `
+<div class="jumbotron">
+  <h4>Respond</h4>
+  <form id="responseInputForm">
+    <div class="form-group">
+      <label for="responseDescInput">Response</label>
+      <input type="text" class="form-control" id="responseInput" placeholder="Enter your response ...">
+    </div>
+    <div class="form-group">
+      <label for="responseDescInput">Name</label>
+      <input type="text" class="form-control" id="nameInput" placeholder="Enter your name ...">
+    </div>
+    <div class="form-group">
+      <label for="responseDescInput">Email</label>
+      <input type="text" class="form-control" id="emailInput" placeholder="Enter your email ...">
+    </div>
+    <button type="submit" class="btn btn-primary" id="submit" disabled>Submit</button>
+  </form>
+</div>
+`;
