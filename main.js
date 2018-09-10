e7d5ee5409e780e651c846bbc445cca3381dad7d
Soren Williams <sorenw@khanlabschool.org>
2018-09-10 16:54:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ if (Date.now() < 1536274800000) {
 }
 
 if (index !== 0) {
-  firebase.database().ref().once('prompts').then(function(snapshot) {
-    document.getElementById('prompt').innerHTML = snapshot[index];
+  firebase.database().ref('prompts/' + index).once().then(function(snapshot) {
+    document.getElementById('prompt').innerHTML = snapshot;
   });
   document.getElementById('submit').disabled = false;
 }
