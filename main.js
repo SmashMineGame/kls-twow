b40a8dd123b442cc7172e3ef4f08121e403b1a6c
Soren Williams <sorenw@khanlabschool.org>
2018-09-10 16:47:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-document.getElementByID('container').innerHTML += respondHTML;
-document.getElementByID('container').innerHTML += voteHTML;
+if (Date.now() > 1536274800000 + 604800000) {
+	var index = 2;
+}
 if (Date.now() > 1536274800000) {
 	var index = 1;
 }
@@ -7,17 +8,12 @@ if (Date.now() < 1536274800000) {
 	var index = 0;
 }
 
-var prompts = [
-	'You have been named president for a day. What do you do?'
-];
-
 if (index !== 0) {
-  document.getElementById('prompt').innerHTML = prompts[index - 1];
+  document.getElementById('prompt').innerHTML = firebase.database().ref('prompts')[index];
   document.getElementById('submit').disabled = false;
 }
 
 document.getElementById('responseInputForm').addEventListener('submit', saveResponse);
-Sortable.create(simpleList, {animation:150});
 
 function saveResponse(e) {
   if (document.getElementById('responseInput').value === '' || document.getElementById('nameInput').value === '' || document.getElementById('emailInput').value === '') {
